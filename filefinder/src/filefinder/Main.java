cac33cad6f43bc56ce09648ecbc5d6450f39ff8e
Eugene <kashevg@gmail.com>
2014-11-19 15:30:01 +0300

    add config

--- filefinder/src/filefinder/Main.java
+++ filefinder/src/filefinder/Main.java
@@ -11,10 +11,6 @@ public class Main {
             System.exit(0);
         } else {
             Config.getInstance().readcfg();
-            for (String str : Config.getInstance().getSTRING_ARRAY_FILE_EXCEPT())
-                System.out.println(str);
-            for (String str :  Config.getInstance().getSTRING_ARRAY_FILE_EXTENSIONS())
-                System.out.println(str);
             FileSearcher fileSearcher = new FileSearcher(args[0], new WriteToConsole());
             fileSearcher.run();
         }
